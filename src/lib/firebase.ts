5c6c9e97b59be35879a37761d68eb3483bd6390b
Dishank0029 <84245762+Dishank0029@users.noreply.github.com>
2024-10-28 22:18:54 +0530

    Initial commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7QHdrczoGzDFO0-yvcopDxgzFkr87o_s",
-    authDomain: "travel-nextjs-ad5e8.firebaseapp.com",
-    projectId: "travel-nextjs-ad5e8",
-    storageBucket: "travel-nextjs-ad5e8.appspot.com",
-    messagingSenderId: "21013038583",
-    appId: "1:21013038583:web:dbad396f49ad4cd29c9af2"
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
